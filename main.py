9a1e6beb76ccec9b82e4be2578535487235918c1
Amit Konforti <amitkonforti123@gmail.com>
2023-02-20 13:30:12 +0200

    final

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ window.title("dadi Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="dadigenerator/background.png")
+background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(150, 207, text="Dadi Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
                                 fill="white")
 canvas.grid(row=0, column=0)
 
-dadi_img = PhotoImage(file="dadigenerator/dadi.png")
+dadi_img = PhotoImage(file="dadi.png")
 kanye_button = Button(image=dadi_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
