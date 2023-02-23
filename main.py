931e16db9b5fd790a2834b97a4f4747d75e3b6d5
Amit Konforti <amitkonforti123@gmail.com>
2023-02-23 15:38:58 +0200

    add quotes

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ quotes = {0: "איזה ארטיק קרח באלך?",
           4: "מי הדליק את הדוד?",
           5: "אני הלכתי",
           6: "היידה בי היידה בו היידה מכבי יפו",
-          7: "למה לא 100?"}
+          7: "למה לא 100?",
+          8:"אתה פישר שלא יודע את מקומו",
+          9:"מיייייי"}
 
 
 def get_quote():
@@ -27,13 +29,13 @@ window.title("dadi Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
+background_img = PhotoImage(file="dadigenerator/background.png")
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(150, 207, text="Dadi Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
                                 fill="white")
 canvas.grid(row=0, column=0)
 
-dadi_img = PhotoImage(file="dadi.png")
+dadi_img = PhotoImage(file="dadigenerator/dadi.png")
 kanye_button = Button(image=dadi_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
